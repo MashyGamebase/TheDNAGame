40ea53657bcfecc7ec9f8d84635c47c3397d1b9f
reirei <zelsevilleta@gmail.com>
2024-03-31 15:52:38 +0800

    Some changes in UI
     -Recombinant
    -Choose what level
    -tried some fonts

--- TheDNAGame/Assets/Scripts/Main/MainMenuControl.cs
+++ TheDNAGame/Assets/Scripts/Main/MainMenuControl.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenuControl : MonoBehaviour
-{
+ {
     public MinigamesButton[] buttons;
 
     private void Start()
@@ -39,20 +39,20 @@ public class MainMenuControl : MonoBehaviour
     }
 
 
-    public void OnClick_Start(int whichLevelToLoad)
+     public void OnClick_Start(int whichLevelToLoad)
     {
         SceneManager.LoadScene(1);
-        GameManager.Instance.sceneToLoad = whichLevelToLoad;
+         GameManager.Instance.sceneToLoad = whichLevelToLoad;
     }
 
     public void OnClick_Options()
     {
-        // might not be needed but only for sound options
+    //might not be needed but only for sound options
     }
 
     public void OnClick_Quit()
     {
-        // run a save data first before quitting
+    // run a save data first before quitting
         Application.Quit();
     }
 }
