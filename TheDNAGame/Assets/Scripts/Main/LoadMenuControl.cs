20333f7434df882c1ed8c68f2862e78db7a66142
reirei <zelsevilleta@gmail.com>
2024-04-02 21:55:04 +0800

    some changes UI

--- TheDNAGame/Assets/Scripts/Main/LoadMenuControl.cs
+++ TheDNAGame/Assets/Scripts/Main/LoadMenuControl.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+// public Slider progressBar;
 
 public class LoadMenuControl : MonoBehaviour
 {
