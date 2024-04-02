a9308fc8b771ffe8ec9cc07432cf4bd65521fc2a
Mashyyyy <76673249+Mashyyyy@users.noreply.github.com>
2024-04-02 20:45:32 +0800

    Bug fixes

--- TheDNAGame/Assets/Scripts/Gameplay/OrganFunctions/OrganMatchObjectives.cs
+++ TheDNAGame/Assets/Scripts/Gameplay/OrganFunctions/OrganMatchObjectives.cs
@@ -3,6 +3,13 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 
+public enum Game
+{
+    A,
+    B
+}
+
+
 public class OrganMatchObjectives : MonoBehaviour
 {
     // 5th minigame
@@ -21,16 +28,27 @@ public class OrganMatchObjectives : MonoBehaviour
     public TextMeshProUGUI questText;
     public float delayBetweenCharacters;
 
+    [Header("Timer Properties")]
+    public float totalTime;
+    private float currentTime;
+    [SerializeField] private TextMeshProUGUI timerText;
+
     private void Awake()
     {
         if (instance == null)
             instance = this;
     }
 
+    private void Start()
+    {
+        currentTime = totalTime;
+    }
+
     private void Update()
     {
         UpdateMatches();
         UpdateScore();
+        UpdateTimer();
     }
 
     void UpdateMatches()
@@ -41,6 +59,31 @@ public class OrganMatchObjectives : MonoBehaviour
     void UpdateScore()
     {
         scoreText.text = score.ToString("D4");
+    }
+
+    void UpdateTimer()
+    {
+        currentTime -= Time.deltaTime;
+
+        currentTime = Mathf.Clamp(currentTime, 0, totalTime);
+
+        int minutes = Mathf.FloorToInt(currentTime / 60f);
+        int seconds = Mathf.FloorToInt(currentTime % 60f);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+
+        if (currentTime <= 0f)
+        {
+            //Lose
+            if (score > 0)
+            {
+                GameManager.Instance.isLevelComplete[levelId] = true;
+            }
+
+            gameWinLose.SetActive(true);
+            //Debug.Log("Timer Ran out!");
+        }
     }
 
     public void AddMatches(int _matches)
